3b7735b6c93758154886c215ac6f63a5c47d52c1
Gobi0516 <egobinath2001@gmail.com>
2025-04-28 16:07:38 +0530

    create controllers

--- src/main/java/com/online/OnlineLearningSystem/Controller/InstructorController.java
+++ src/main/java/com/online/OnlineLearningSystem/Controller/InstructorController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
+//hi
 @RestController
 @RequestMapping("/api/instructors")
 public class InstructorController {
--- src/main/java/com/online/OnlineLearningSystem/Controller/StudentController.java
+++ src/main/java/com/online/OnlineLearningSystem/Controller/StudentController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
-
+//hi
 @RestController
 @RequestMapping("/api/students")
 public class StudentController {
