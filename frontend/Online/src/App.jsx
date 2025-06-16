93a710fa0058004178e9f793282f72d2b74d8356
Gobi0516 <egobinath2001@gmail.com>
2025-06-16 11:58:57 +0530

    frontend intit comit

---
+++ frontend/Online/src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+  
+  return (
+    <div>
+      <div>Login</div>
+      <div>Sigin</div>
+      <h1>Welcome to the Online Learning System</h1>
+    </div>
+  )
+}
+
+export default App
